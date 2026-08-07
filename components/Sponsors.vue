7baf33f6108c1e44af0110981e96d842b55e90da
David Nguyen <h250694@gmail.com>
2026-08-07 16:20:09 +0700

    Update sponsor link to ePlus.DEV

--- components/Sponsors.vue
+++ components/Sponsors.vue
@@ -1,26 +1,13 @@
 <template>
   <div class="items-center justify-center gap-x-2 sm:gap-x-4 md:gap-x-8 hidden sm:flex">
     <a
-      href="https://github.com/sponsors/jbrooksuk"
-      title="Sponsor James Brooks"
-      @click="sponsorClick"
+      href="https://github.com/sponsors/ePlus-DEV"
+      title="Sponsor ePlus.DEV"
       target="_blank"
       rel="noopener noreferrer"
       class="text-artisan hover:text-artisan-light text-sm font-bold"
     >
-      Sponsor Artisan.page on GitHub
+      Sponsor ePlus.DEV on GitHub
     </a>
   </div>
 </template>
-
-<script>
-export default {
-  methods: {
-    sponsorClick() {
-      if (typeof window.fathom !== 'undefined') {
-        window.fathom.trackGoal('L3DZXKHP', 0)
-      }
-    },
-  },
-}
-</script>
